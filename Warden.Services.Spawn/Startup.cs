2caccee8763f328b4934ed6a7ca891f4ba529a4e
spetz <spetz90@gmail.com>
2017-02-07 12:34:36 +0100

    Updated project.json packages and NLog configuration.

--- Warden.Services.Spawn/Startup.cs
+++ Warden.Services.Spawn/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Nancy.Owin;
 using NLog.Extensions.Logging;
+using NLog.Web;
 using Warden.Services.Spawn.Framework;
 
 namespace Warden.Services.Spawn
